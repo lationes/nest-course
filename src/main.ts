7694e7d2aa4b0e54bfd8879e74c79fde18b8960e
lationes <danyil.dmytriev.ideus@gmail.com>
2023-11-27 17:45:44 +0200

    nest-course

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as process from "process";
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+}
+
+bootstrap();
